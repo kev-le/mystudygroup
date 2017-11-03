bdbb19d746c0d4b886d9bc6d9562cfea0c16402b
kevin-le111@hotmail.com <kevin-le111@hotmail.com>
2017-11-03 02:16:04 -0700

    Set session variables on user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,10 @@ class UsersController < ApplicationController
     if @user.save
       # Automatically sign in user
       session[:user_id] = @user.id
-
+      session[:user_id] = @user.id
+      session[:email] = @user.email
+      session[:first_name] = @user.first_name
+      session[:last_name] = @user.last_name
       redirect_to root_url_path, notice: 'Successfully signed up!'
     else
       render :new
