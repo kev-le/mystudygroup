0919d23f734e61939db467b75616fd7acbffa075
kevin-le111@hotmail.com <kevin-le111@hotmail.com>
2017-11-20 00:21:33 -0800

    Fix course search

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,8 +1,8 @@
 $(function() {
   $('#search').on('keyup', function() {
     var pattern = $(this).val();
-    $('.searchable-container .row .items').hide();
-    $('.searchable-container .row .items').filter(function() {
+    $('.searchable-container form .row .items').hide();
+    $('.searchable-container form .row .items').filter(function() {
       return $(this).find('.course-tag').text().match(new RegExp(pattern, 'i'));
     }).show();
   });
