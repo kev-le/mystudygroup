1a28b4cf1190af557624b3af6d823c91da6ef681
Kevin Le <kevin.le@ayogo.com>
2017-11-20 13:54:37 -0800

    Fix course search by using turbolinks

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,9 +1,11 @@
-$(function() {
-  $('#search').on('keyup', function() {
-    var pattern = $(this).val();
-    $('.searchable-container form .row .items').hide();
-    $('.searchable-container form .row .items').filter(function() {
-      return $(this).find('.course-tag').text().match(new RegExp(pattern, 'i'));
-    }).show();
+document.addEventListener("turbolinks:load", function() {
+  $(function() {
+    $('#search').on('keyup', function() {
+      var pattern = $(this).val();
+      $('.searchable-container form .row .items').hide();
+      $('.searchable-container form .row .items').filter(function() {
+        return $(this).find('.course-tag').text().match(new RegExp(pattern, 'i'));
+      }).show();
+    });
   });
-});
+})
