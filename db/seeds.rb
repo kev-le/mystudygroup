bf0623c59f44c35acfdb5e948b22150250edb547
Kevin Le <kevin.le@ayogo.com>
2017-11-20 18:01:13 -0800

    Add new seeds and fix course enrol

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,18 @@ u1 = User.create("first_name" => "Bobby",
               "password" => "123456",
               "password_confirmation" => "123456")
 #courses
-c1 = Course.create("name" => "CMPT 276", "description" => "intro to software development")
-c2 = Course.create("name" => "CMPT 218", "description" => "intro to web development")
+c1 = Course.create("name" => "CMPT 276", "description" => "Intro to software development")
+c2 = Course.create("name" => "CMPT 218", "description" => "Intro to web development")
+c3 = Course.create("name" => "CMPT 318", "description" => "Intro to artificial intelligence")
+c4 = Course.create("name" => "CMPT 295", "description" => "Intro to computer systems")
+c5 = Course.create("name" => "CMPT 120", "description" => "Intro to computer science and programming")
+c6 = Course.create("name" => "CMPT 125", "description" => "Intro to computer science and programming II")
+c7 = Course.create("name" => "MACM 101", "description" => "Discrete Mathematics I")
+c8 = Course.create("name" => "MACM 201", "description" => "Discrete Mathematics II")
+c9 = Course.create("name" => "MACM 101", "description" => "Discrete Mathematics I")
+c10 = Course.create("name" => "MATH 150", "description" => "Calculus I")
+c11 = Course.create("name" => "MATH 152", "description" => "Calculus II")
+
 
 # groups
 g1 = Group.create("course_id" => c1.id, "name" => "A fun group!", "description" => "exciting group to study with! Come join!")
