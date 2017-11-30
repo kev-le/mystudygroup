d19b5d253f50fc06b5dfdaa57aa4b3a0e5395601
Kevin Le <kevin.le@ayogo.com>
2017-11-29 17:46:34 -0800

    add more courses to study spot

--- app/controllers/study_spot_controller.rb
+++ app/controllers/study_spot_controller.rb
@@ -1,20 +1,32 @@
 class StudySpotController < ApplicationController
   def index
-    # get the user enrollments
-    enrollments = Enrollment.where("user_id" => session[:user_id])
-    @groups = []
 
-    # get the user groups
-    enrollments.each do |enrollment|
-      if (enrollment.group_id.blank?)
-        next
+    # get the courses of the user and put into array
+    courses = Enrollment.where("user_id" => session[:user_id], "group_id" => nil).pluck(:course_id)
+
+    # find all groups based on courses
+    enrollments = []
+    if (courses)
+      courses.each do |id|
+        enrollment = Enrollment.where("course_id" => id).where.not("group_id" => nil)
+        if courses
+          enrollments.push(*enrollment)
+        end
       end
+    end
+
+
+    # get the groups for courses that user is enrolled in
+    @groups = []
+    enrollments.each do |enrollment|
       group = Group.find_by("id" => enrollment.group_id)
       @groups.push(group)
     end
 
     if (@groups.empty?)
       @empty = "No available study spots. Did you enroll in any courses?"
+    else
+      @group_ids = Enrollment.where("user_id" => session[:user_id]).pluck(:group_id)
     end
   end
 end
