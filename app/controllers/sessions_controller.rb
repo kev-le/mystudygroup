73279a936cc9a8d0b301b5a96fcf49dd1537f205
Kevin Le <kevin.le@ayogo.com>
2017-11-30 17:56:01 -0800

    Add logged in/out messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       session[:first_name] = user.first_name
       session[:last_name] = user.last_name
       session[:role] = user.role
-      redirect_to root_url_path
+
+      redirect_to root_url_path, notice: 'Logged in!'
     else
       flash.now.alert = 'Email or password is invalid'
       render :new
