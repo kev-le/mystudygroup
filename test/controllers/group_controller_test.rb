14434a96c3a8734e3143ddd1918983982893740c
Kevin Le <kevin.le@ayogo.com>
2017-12-04 10:25:33 -0800

    Add group controller test

--- test/controllers/group_controller_test.rb
+++ test/controllers/group_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class GroupControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @course = courses(:one)
+    @user = users(:one)
+
+    # Log a user in to create a session before testing
+    post "/sessions", params: { email: @user.email, password: "password"}
+  end
+
+  test "should create group" do
+    assert_difference("Group.count") do
+      post "/addagroup/create", params: { group: { course_id: @course.id, name: "Test Group", description: "test description" } }
+    end
+
+    assert_redirected_to("/groups")
+  end
 end
